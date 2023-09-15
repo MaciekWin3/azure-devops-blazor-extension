1402a8552f984cf1e68619060a52d2905908acd4
maciekwin3 <maciekwin3@gmail.com>
2023-09-15 15:55:24 +0200

    Improve build script

--- src/AzDevOpsBlazorExtension/build.ps1
+++ src/AzDevOpsBlazorExtension/build.ps1
@@ -3,11 +3,14 @@ $projectPath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
 $destinationFolder = Join-Path $projectPath "dist"
 $publishFolder = Join-Path $projectPath "bin\Debug\net7.0\publish\wwwroot"
 dotnet publish $projectPath -o $publishFolder  | Out-Null
-Write-Host "Build completed..."
+Write-Host "Build done..."
 Write-Host "Coping project files to dist folder..."
 if (-not (Test-Path -Path $destinationFolder -PathType Container)) {
     New-Item -Path $destinationFolder -ItemType Directory -Force
 }
 Copy-Item -Path "$publishFolder\*" -Destination $destinationFolder -Recurse -Force | Out-Null
-Write-Host "Publishing and copying complete."
+Write-Host "Coping project files done..."
+Write-Host "Creating extension..."
+tfx extension create
+Write-Host "Creating extension done."
 
