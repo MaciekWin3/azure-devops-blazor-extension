87ef96793261b0302be2f9b1143659cc73f31fbb
maciekwin3 <maciekwin3@gmail.com>
2023-09-19 09:34:23 +0200

    Fix build script

--- src/AzDevOpsBlazorExtension/build.ps1
+++ src/AzDevOpsBlazorExtension/build.ps1
@@ -1,15 +1,16 @@
 Write-Host "Building project..."
 $projectPath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
 $destinationFolder = Join-Path $projectPath "dist"
-$publishFolder = Join-Path $projectPath "bin\Debug\net7.0\publish\wwwroot"
-dotnet publish $projectPath -o $publishFolder  | Out-Null
+$publishFolder = Join-Path $projectPath "bin\Release\net7.0\publish\wwwroot"
+dotnet publish --configuration Release | Out-Null
 Write-Host "Build done..."
-Write-Host "Coping project files to dist folder..."
+Write-Host "Copying project files to dist folder..."
 if (-not (Test-Path -Path $destinationFolder -PathType Container)) {
     New-Item -Path $destinationFolder -ItemType Directory -Force
 }
 Copy-Item -Path "$publishFolder\*" -Destination $destinationFolder -Recurse -Force | Out-Null
-Write-Host "Coping project files done..."
+Write-Host "Copying project files done..."
+
 Write-Host "Creating extension..."
 $extensionCommand = "tfx extension create"
 if ($args -contains "--dev") {
