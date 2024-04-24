029889729869b5bf234fc337fa1be40322d2aad5
Maciej Winnik <maciekwin3@gmail.com>
2024-04-24 20:26:11 +0200

    Update to .NET 8

--- src/AzDevOpsBlazorExtension/build.ps1
+++ src/AzDevOpsBlazorExtension/build.ps1
@@ -1,8 +1,14 @@
 Write-Host "Building project..."
 $projectPath = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
 $destinationFolder = Join-Path $projectPath "dist"
-$publishFolder = Join-Path $projectPath "bin\Release\net7.0\publish\wwwroot"
-dotnet publish --configuration Release | Out-Null
+if ($args -contains "--dev") {
+	$publishFolder = Join-Path $projectPath "bin\Debug\net8.0\publish\wwwroot"
+	dotnet publish --configuration Debug | Out-Null
+}
+else {
+	$publishFolder = Join-Path $projectPath "bin\Release\net8.0\publish\wwwroot"
+	dotnet publish --configuration Release | Out-Null
+}
 Write-Host "Build done..."
 Write-Host "Copying project files to dist folder..."
 if (-not (Test-Path -Path $destinationFolder -PathType Container)) {
